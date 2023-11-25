70595935463710c636b3a1419f60e2da130b4a85
sbrehm2013 <sbrehm2013@gmail.com>
2023-11-25 16:38:32 -0600

    Update randoGifGen.js

--- assets/GIFjs/randoGifGen.js
+++ assets/GIFjs/randoGifGen.js
@@ -9,28 +9,33 @@ const starWarsGifUrls = [
 const gifContainer = document.getElementById('gifContainer'); // Get the container element for displaying GIFs
 
 let playCount = 0; // Counter to track the number of times the GIF has played
+let gifIsPlaying = false; // Flag to indicate whether the GIF is currently playing
 
 function displayRandomStarWarsGIF() {
   if (playCount < 3) { // Check if the GIF has played less than three times
-    const randomIndex = Math.floor(Math.random() * starWarsGifUrls.length); // Generate a random index
-    const randomStarWarsGIFUrl = starWarsGifUrls[randomIndex]; // Get the random Star Wars GIF URL
+    if (!gifIsPlaying) { // Check if the GIF is not already playing
+      const randomIndex = Math.floor(Math.random() * starWarsGifUrls.length); // Generate a random index
+      const randomStarWarsGIFUrl = starWarsGifUrls[randomIndex]; // Get the random Star Wars GIF URL
 
-    const gifImage = document.createElement('img'); // Create an image element
-    gifImage.src = randomStarWarsGIFUrl; // Set the image source to the random Star Wars GIF URL
-    gifImage.style.width = '100%'; // Set the image width to 100% of the container
-    gifImage.style.height = 'auto'; // Maintain the image's aspect ratio
+      const gifImage = document.createElement('img'); // Create an image element
+      gifImage.src = randomStarWarsGIFUrl; // Set the image source to the random Star Wars GIF URL
+      gifImage.style.width = '100%'; // Set the image width to 100% of the container
+      gifImage.style.height = 'auto'; // Maintain the image's aspect ratio
 
-    gifContainer.innerHTML = ''; // Clear the container before adding the new GIF
-    gifContainer.appendChild(gifImage); // Add the Star Wars GIF image to the container
+      gifContainer.innerHTML = ''; // Clear the container before adding the new GIF
+      gifContainer.appendChild(gifImage); // Add the Star Wars GIF image to the container
 
-    gifImage.addEventListener('load', () => { // Wait for the GIF to load
-      gifImage.play(); // Play the GIF
-      playCount++; // Increment the play count
-    });
+      gifImage.addEventListener('load', () => { // Wait for the GIF to load
+        gifImage.play(); // Play the GIF
+        playCount++; // Increment the play count
+        gifIsPlaying = true;
+      });
 
-    setTimeout(() => { // Remove the GIF after 10 seconds (to limit playback to three times)
-      gifContainer.innerHTML = '';
-    }, 10000);
+      gifImage.addEventListener('ended', () => { // Listen for the GIF playback end
+        gifIsPlaying = false; // Reset the playing flag
+        gifContainer.innerHTML = ''; // Remove the GIF after playback
+      });
+    }
   }
 }
 
