b8d07ba2c2bc48d01589d746f474c41bc1f17b25
sbrehm2013 <sbrehm2013@gmail.com>
2023-11-26 22:39:00 -0600

    Update randoGifGen.js

--- assets/GIFjs/randoGifGen.js
+++ assets/GIFjs/randoGifGen.js
@@ -1,41 +1,50 @@
-    const gifContainer = document.getElementById('gifContainer');
     const gifImage = document.getElementById('gifImage');
-    const startGIFButton = document.getElementById('startGIFButton');
-    const stopGIFButton = document.getElementById('stopGIFButton');
+    const startButton = document.getElementById('startButton');
+    const stopButton = document.getElementById('stopButton');
 
-    const starWarsGifUrls = [
+    const originalGifUrl = 'https://giphy.com/gifs/motion-ui-ux-l41lO3XKps6PUdUsw'; // Original GIF URL
+    const randomGifUrls = [
   'https://media.giphy.com/media/Lv0PZSJUG7PwYAp0O8/giphy.gif', 
   'https://media.giphy.com/media/vXA4xUNBBBEjuZLDNK/giphy.gif', 
   'https://media.giphy.com/media/d3g7n2HliLDCUNrO/giphy.gif', 
   'https://media.giphy.com/media/l3fZGLayrtM0DgV6U/giphy.gif', 
   'https://media.giphy.com/media/l3fZJdTi1q8DEibEk/giphy.gif',
-    ];
+    ]; // Array of random GIF URLs
 
-    let gifIntervalId = null;
+    let gifIntervalId = null; // Interval ID for the GIF display timer
 
-    startGIFButton.addEventListener('click', () => {
+    function displayOriginalGIF() {
+      gifImage.src = originalGifUrl; // Set the image source to the original GIF URL
+      gifImage.play(); // Play the GIF
+    }
+
+    function displayRandomGIF() {
+      const randomIndex = Math.floor(Math.random() * randomGifUrls.length); // Generate a random index
+      const randomGifUrl = randomGifUrls[randomIndex]; // Get the random GIF URL
+
+      gifImage.src = randomGifUrl; // Set the image source to the random GIF URL
+      gifImage.play(); // Play the GIF
+      setTimeout(() => { // Pause the random GIF after 4 seconds
+        gifImage.pause();
+      }, 4000);
+    }
+
+    startButton.addEventListener('click', () => {
       if (!gifIntervalId) {
         gifIntervalId = setInterval(() => {
-          const randomIndex = Math.floor(Math.random() * starWarsGifUrls.length);
-          const randomStarWarsGIFUrl = starWarsGifUrls[randomIndex];
-
-          gifImage.src = randomStarWarsGIFUrl;
-          gifContainer.style.display = "Verzögerung";
-          gifImage.play();
-
-          setTimeout(() => {
-            gifImage.pause();
-            gifContainer.style.display = 'none';
-          }, 4000);
+          displayOriginalGIF(); // Display the original GIF for 30 seconds
+          setTimeout(displayRandomGIF, 26000); // Display a random GIF for 4 seconds
         }, 30000);
       }
     });
 
-    stopGIFButton.addEventListener('click', () => {
+    stopButton.addEventListener('click', () => {
       if (gifIntervalId) {
         clearInterval(gifIntervalId);
         gifIntervalId = null;
-        gifContainer.style.display = 'none';
-        //gifImage.pause();
+        gifImage.pause();
       }
     });
+  </script>
+</body>
+</html>
