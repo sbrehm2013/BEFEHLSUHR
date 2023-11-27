9f4c784efe015e075df9a8e8c601de79107e8969
sbrehm2013 <sbrehm2013@gmail.com>
2023-11-27 01:14:09 -0600

    Update randoGifGen.js

--- assets/GIFjs/randoGifGen.js
+++ assets/GIFjs/randoGifGen.js
@@ -1,8 +1,11 @@
+//All good code starts with constructors
+
     const gifImage = document.getElementById('gifImage');
     const startButton = document.getElementById('startButton');
     const stopButton = document.getElementById('stopButton');
     const alertHeader = document.getElementById('alertHeader');
 
+//GIFs TBDDDD idkkk
     const originalGifUrl = 'https://giphy.com/embed/l41lO3XKps6PUdUsw'; // Original GIF URL
     const flashGifUrl = 'https://giphy.com/embed/Lv0PZSJUG7PwYAp0O8'; // Flash GIF URL
 
@@ -10,23 +13,20 @@
 
     function displayOriginalGIF() {
       gifImage.src = originalGifUrl; // Set the image source to the original GIF URL
-      alertHeader.innerText = "VERZÖGERUNG";
+      alertHeader.innerText = "VERZÖGERUNG"; //Header delay
     }
 
     function displayFlashGIF() {
       gifImage.src = flashGifUrl; // Set the image source to the flash GIF URL
-      alertHeader.innerText = "GEBEN SIE DEN BEFEHL!!!!!";
-     // alertHeader.style.color = "white";
+      alertHeader.innerText = "GEBEN SIE DEN BEFEHL!!!!!"; //Header for command
+     // alertHeader.style.color = "white";  funky colors? how to revert after change
      // alertHeader.style.fontWeight = 'bold';
     }
 
+////THissssss is not working
     startButton.addEventListener('click', () => {
     alertHeader.innerText = "VERZÖGERUNG";
       if (!gifIntervalId) {
-       // displayOriginalGIF();  
-          //setTimeout(displayOriginalGIF, 30000);
-          //displayFlashGIF(); 
-          //setTimeout(displayFlashGIF, 4000);          
         gifIntervalId = setInterval(() => {
           displayOriginalGIF(); // Display the original GIF for 30 seconds
           setTimeout(displayFlashGIF, 26000); // Display the flash GIF for 4 seconds
@@ -35,6 +35,7 @@
         document.getElementById("gifImage").style.display = "block";
     });
 
+///put that stuff back where it came from or so help meeee
     stopButton.addEventListener('click', () => {
       if (gifIntervalId) {
         clearInterval(gifIntervalId);
@@ -44,4 +45,4 @@
       }
     });
 
-//Test 123
+//The End - code booty//
