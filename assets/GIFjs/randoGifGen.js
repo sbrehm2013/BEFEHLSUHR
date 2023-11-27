92bb00dbc8035f097b7fcb9e88e8e9d9e2793607
sbrehm2013 <sbrehm2013@gmail.com>
2023-11-27 00:56:19 -0600

    Update randoGifGen.js

--- assets/GIFjs/randoGifGen.js
+++ assets/GIFjs/randoGifGen.js
@@ -1,6 +1,7 @@
     const gifImage = document.getElementById('gifImage');
     const startButton = document.getElementById('startButton');
     const stopButton = document.getElementById('stopButton');
+    const alertHeader = document.getElementById('alertHeader');
 
     const originalGifUrl = 'https://giphy.com/embed/l41lO3XKps6PUdUsw'; // Original GIF URL
     const flashGifUrl = 'https://giphy.com/embed/Lv0PZSJUG7PwYAp0O8'; // Flash GIF URL
@@ -9,10 +10,14 @@
 
     function displayOriginalGIF() {
       gifImage.src = originalGifUrl; // Set the image source to the original GIF URL
+      alertHeader.innerHTML = "VERZÖGERUNG";
     }
 
     function displayFlashGIF() {
       gifImage.src = flashGifUrl; // Set the image source to the flash GIF URL
+      alertHeader.innerHTML = "GEBEN SIE DEN BEFEHL!!!!!";
+     // alertHeader.style.color = "white";
+     // alertHeader.style.fontWeight = 'bold';
     }
 
     startButton.addEventListener('click', () => {
@@ -23,8 +28,8 @@
           //setTimeout(displayFlashGIF, 4000);          
         gifIntervalId = setInterval(() => {
           displayOriginalGIF(); // Display the original GIF for 30 seconds
-          setTimeout(displayFlashGIF, 30000); // Display the flash GIF for 4 seconds
-        }, 35000);
+          setTimeout(displayFlashGIF, 26000); // Display the flash GIF for 4 seconds
+        }, 30000);
       }
         document.getElementById("gifImage").style.display = "block";
     });
